1f2f12ae91101cf371bb102db617e011d2e5ac24
houssamos <touilhoussem@yahoo.fr>
2025-06-24 22:33:20 +0200

    Update cultures.service.js
    delete export mapping

--- src/services/cultures.service.js
+++ src/services/cultures.service.js
@@ -1,8 +1,6 @@
 const culturesRepository = require("../repositories/cultures.repository");
 const { entityToModel } = require("../mapping/culture.mapping");
 
-exports.toModel = entityToModel;
-
 exports.getAllCultures = async () => {
     const entities = await culturesRepository.findAll();
     return entities.map(entityToModel);
