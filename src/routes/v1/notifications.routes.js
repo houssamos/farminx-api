253d007ff32c9c04f79f2199813e86721dd550b3
Houssem TOUIL <houssem.touil@outlook.com>
2025-08-11 23:25:28 +0200

    add userAuth to notification

--- src/routes/v1/notifications.routes.js
+++ src/routes/v1/notifications.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const notificationsController = require('../../controllers/notifications.controller');
+const userAuth = require('../../middlewares/user-auth.middleware');
 
-router.post('/subscribe', notificationsController.subscribe);
-router.get('/', notificationsController.get);
+
+router.post('/subscribe', userAuth, notificationsController.subscribe);
+router.get('/', userAuth, notificationsController.get);
 
 module.exports = router;
