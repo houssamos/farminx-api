89994d59138fa1074ac986de71784633a439875c
houssamos <touilhoussem@yahoo.fr>
2025-08-24 21:48:53 +0200

    feat: add HTML mail utility

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,1 +1,27 @@
-x
+const fs = require('fs');
+const handlebars = require('handlebars');
+const { transporter } = require('../services/email.service');
+
+/**
+ * Sends an HTML email based on a Handlebars template.
+ *
+ * @param {Object} options
+ * @param {string} options.to Recipient email address
+ * @param {string} options.subject Email subject
+ * @param {string} options.templatePath Path to the Handlebars template file
+ * @param {Object} [options.variables={}] Variables to interpolate in the template
+ */
+async function sendHtmlNotification({ to, subject, templatePath, variables = {} }) {
+  const templateSource = fs.readFileSync(templatePath, 'utf8');
+  const template = handlebars.compile(templateSource);
+  const html = template(variables);
+
+  await transporter.sendMail({
+    from: process.env.EMAIL_SENDER,
+    to,
+    subject,
+    html,
+  });
+}
+
+module.exports = { sendHtmlNotification };
